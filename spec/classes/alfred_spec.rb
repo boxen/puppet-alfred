cb74b545962a5c028db4b6a3d48667161f638150
Mark Tareshawty <tarebyte@gmail.com>
2014-03-23 22:52:42 -0400

    whitespace

--- spec/classes/alfred_spec.rb
+++ spec/classes/alfred_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 classes = {
-	
   'alfred'     => 'http://cachefly.alfredapp.com/Alfred_2.2_243b.zip',
   'alfred::v1' => 'http://cachefly.alfredapp.com/alfred_1.4_268.zip'
 }
