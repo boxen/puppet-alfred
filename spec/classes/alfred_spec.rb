d9d179074aff9d2b5e13b77f9549275aabac8008
Pratham Agrawal <mail@prath.am>
2014-08-16 04:07:29 +0530

    fixed the spec to match updated version

--- spec/classes/alfred_spec.rb
+++ spec/classes/alfred_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'alfred' do
   it do
     should contain_package('Alfred 2').with({
-      :source   => 'http://cachefly.alfredapp.com/Alfred_2.2_243b.zip',
+      :source   => 'http://cachefly.alfredapp.com/Alfred_2.4_279.zip',
       :provider => 'compressed_app'
     })
   end
