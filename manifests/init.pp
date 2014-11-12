d5aea0d01b5805938d149349f0f8b7a43c030f98
Robert Burns <burnsra@gmail.com>
2014-11-12 15:15:34 -0500

    Update to alfred version 2.5.1_308

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include alfred
 class alfred (
-  $version = '2.5_299'
+  $version = '2.5.1_308'
 ) {
   package { 'Alfred 2':
     provider => 'compressed_app',
--- spec/classes/alfred_spec.rb
+++ spec/classes/alfred_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'alfred' do
   it do
     should contain_package('Alfred 2').with({
-      :source   => 'http://cachefly.alfredapp.com/Alfred_2.4_279.zip',
+      :source   => 'http://cachefly.alfredapp.com/Alfred_2.5.1_308.zip',
       :provider => 'compressed_app'
     })
   end
